0349ed4dd35ef12e88871f60f8cc7ef47f8f726c
Холодилов Александр <shurikhol@mail.ru>
2021-01-27 22:00:17 +0300

    Правка стилизации

--- build/js/main.js
+++ build/js/main.js
@@ -6,11 +6,10 @@ const mainHeaderButton = mainHeader.querySelector(".main-header__button");
 const menuCompact = document.querySelector(".menu-compact");
 const menuCompactClose = menuCompact.querySelector(".menu-compact__close");
 
-const detailCards = document.querySelectorAll(".detail__card");
-const tarifsItems = document.querySelectorAll(".tarifs__item");
-
-const modalOrder = document.querySelector(".modal-order");
-const modalOk = document.querySelector(".modal-ok");
+const orderForm = document.querySelector(".order__form");
+const orderName = orderForm.querySelector(".order__name");
+const orderTelephone = orderForm.querySelector(".order__telephone");
+const orderSubmit = orderForm.querySelector(".order__submit");
 
 const onClickMainHeaderButton = function () {
   menuCompact.classList.remove("menu-compact--closed");
@@ -28,37 +27,9 @@ const onClickMenuCompactClose = function () {
   mainHeaderButton.classList.remove("main-header__button--clicked");
 };
 
-const onClickButtonToSell = function (evt) {
-
-  const modalClose = modalOrder.querySelector(".modal__close");
-  const telephone = modalOrder.querySelector(".modal-order__telephone");
-  const email = modalOrder.querySelector(".modal-order__email");
-  const submit = modalOrder.querySelector(".modal-order__submit");
-
-  const onClickModalClose = function (evt) {
-    onModalClose();
-  }
-
-  const onEscapeModalCard = (evt) => {
-    if (evt.key === `Escape`) {
-      onModalClose();
-    }
-  };
-
-  const onModalClose = function () {
-    modalOrder.classList.remove("modal-show");
-    modalClose.removeEventListener('click', onClickModalClose);
-    document.removeEventListener(`keydown`, onEscapeModalCard);
-  }
-
-  evt.preventDefault();
-
-  modalOrder.classList.add("modal-show");
-  telephone.focus();
-
-  modalClose.addEventListener('click', onClickModalClose);
-  document.addEventListener(`keydown`, onEscapeModalCard);
-}
+const onClickOrderSubmit = function (evt) {
+  // evt.preventDefault();
+};
 
 const initSite = () => {
 
@@ -66,24 +37,10 @@ const initSite = () => {
     mainHeaderButton.classList.remove("main-header__button--no-js");
     menuCompactClose.classList.remove("menu-compact__close--no-js");
     menuCompact.classList.add("menu-compact--closed");
-
-    for (let i=0; i< detailCards.length; i++) {
-      detailCards[i].classList.remove("detail__card--for-pixel-perfect");
-      detailCards[i].classList.remove("detail__card--no-js");
-      let buttonBuy = detailCards[i].querySelector(".detail__to-sell");
-      buttonBuy.addEventListener("click", onClickButtonToSell);
-    }
-
-    for (let i=0; i< tarifsItems.length; i++) {
-      tarifsItems[i].addEventListener("click", onClickButtonToSell);
-      let buttonBuy = tarifsItems[i].querySelector(".tarifs__button");
-      if (buttonBuy) {
-        buttonBuy.addEventListener("click", onClickButtonToSell);
-      }
-    }
   }
 
   mainHeaderButton.addEventListener("click", onClickMainHeaderButton);
+  orderSubmit.addEventListener("click", onClickOrderSubmit);
 };
 
 window.addEventListener(`load`, initSite);
--- source/js/main.js
+++ source/js/main.js
@@ -6,6 +6,11 @@ const mainHeaderButton = mainHeader.querySelector(".main-header__button");
 const menuCompact = document.querySelector(".menu-compact");
 const menuCompactClose = menuCompact.querySelector(".menu-compact__close");
 
+const orderForm = document.querySelector(".order__form");
+const orderName = orderForm.querySelector(".order__name");
+const orderTelephone = orderForm.querySelector(".order__telephone");
+const orderSubmit = orderForm.querySelector(".order__submit");
+
 const onClickMainHeaderButton = function () {
   menuCompact.classList.remove("menu-compact--closed");
   menuCompactClose.addEventListener("click", onClickMenuCompactClose);
@@ -22,6 +27,10 @@ const onClickMenuCompactClose = function () {
   mainHeaderButton.classList.remove("main-header__button--clicked");
 };
 
+const onClickOrderSubmit = function (evt) {
+  // evt.preventDefault();
+};
+
 const initSite = () => {
 
   if (mainHeaderButton.classList.contains("main-header__button--no-js")) {
@@ -31,6 +40,7 @@ const initSite = () => {
   }
 
   mainHeaderButton.addEventListener("click", onClickMainHeaderButton);
+  orderSubmit.addEventListener("click", onClickOrderSubmit);
 };
 
 window.addEventListener(`load`, initSite);
